ee96fbad14c67f40314fb5f210b6b5493bd4342c
Kári S Friðriksson <karibondi@gmail.com>
2020-01-09 12:22:08 +0000

    split in two files

---
+++ R/seasonal_adjustment.R
@@ -0,0 +1,37 @@
+# seasonal ----------------------------------------------------------------
+
+tidy_seas <- function(x, date, frequency = 12, ...){
+  require(lubridate)
+  require(broom)
+  start <- min(date)
+  start <- year(start) + month(start) / 12
+  x <- ts(x, start = start, frequency = frequency)
+
+  x %>%
+    seas(...) %>%
+    pluck("data") %>%
+    as_tibble()
+
+}
+
+nested_seas <- function(data, x, date, frequency = 12, ...){
+  x <- enquo(x)
+  date <- enquo(date)
+
+  tx <- data %>% select(!!x) %>%  pull(1)
+  date_series <- data %>% select(!!date) %>% pull(1)
+
+  tidy_seas(tx, date_series, frequency = frequency, ...) %>%
+    bind_cols(data %>% select(-!!date))
+
+}
+
+map_seas <- function(data, var, date){
+  var <- enquo(var)
+  date <- enquo(date)
+  data %>%
+    nest() %>%
+    mutate(seas = map(data, ~nested_seas(., !!var, !!date))) %>%
+    select(-data) %>%
+    unnest(c(seas))
+}
