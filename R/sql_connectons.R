f9e88065718a9e8f41e670e8c9359af388827ad0
Hannah <hannah.patrick@rittmanmead.com>
2020-01-31 14:42:00 +0000

    no message

--- R/sql_connectons.R
+++ R/sql_connectons.R
@@ -43,9 +43,9 @@ get_connection_HMS <- function(dbhost = "sql02", dname="HgrDwh"){
 #' @export get_connection_JDBC
 #'
 #' @examples conn <- HMSr::get_connection_JDBC(dbcp="D:/R/Drivers/mssql-jdbc-7.2.1.jre8.jar',user="Bob",password ="Password01")
-
-get_connection_JDBC <- function(dbhost = "sql02", dbname="HgrDwh",dbcp="D:/R/Drivers/mssql-jdbc-7.2.1.jre8.jar",user,password){
-  drv <- JDBC(driverClass="com.microsoft.sqlserver.jdbc.SQLServerDriver", classPath=dbcp)
-  conn <- dbConnect(drv, url=sprintf("jdbc:sqlserver://%s;DatabaseName=%s",dbhost,dbname), user=user, password=password)
-  return (conn)
-}
+#
+# get_connection_JDBC <- function(dbhost = "sql02", dbname="HgrDwh",dbcp="D:/R/Drivers/mssql-jdbc-7.2.1.jre8.jar",user,password){
+#   drv <- JDBC(driverClass="com.microsoft.sqlserver.jdbc.SQLServerDriver", classPath=dbcp)
+#   conn <- dbConnect(drv, url=sprintf("jdbc:sqlserver://%s;DatabaseName=%s",dbhost,dbname), user=user, password=password)
+#   return (conn)
+# }
