3543ab7fc84284bdb5b1851286bfb67923f61e00
Borkur Steingrimsson <borkur.steingrimsson@rittmanmead.com>
2020-01-31 09:40:28 +0000

    updated documentation for get_connection functions

--- R/sql_connectons.R
+++ R/sql_connectons.R
@@ -6,20 +6,43 @@
 #' @import DBI
 #' @import RJDBC
 #' @export get_connection_windows
-#' @export get_connection_HMS
-#' @export get_connection_JDBC
-#' @examples
+#' @export
+#' @export
+#' @examples get_connection_windows()
 get_connection_windows <- function(db = "HgrDwh_Prod"){
   dbConnect(odbc(), db, timeout = 10, encoding = "WINDOWS-1252")
 }
 
+#' get_connection_HMS Uses ODBC to connect directly to a named server. Uses Windows Authenication.
+#'
+#' @param dbhost SQL Server hostname
+#' @param dname  SQL Databse name
+#'
+#' @return DBI conection object
+#' @export get_connection_HMS
+#'
+#' @examples HMSr::get_connection_HMS('sql02','HgrDwh')
+#'
 get_connection_HMS <- function(dbhost = "sql02", dname="HgrDwh"){
   return (  dbConnect(odbc::odbc(),
-                      .connection_string = sprintf('driver={SQL Server};server=%s;database=%s;trusted_connection=true',dbhost,dbname),
-                      timeout = 10, encoding = "WINDOWS-1252"
-                      )
+            .connection_string = sprintf('driver={SQL Server};server=%s;database=%s;trusted_connection=true',dbhost,dbname),
+            timeout = 10, encoding = "WINDOWS-1252"
+          )
   )
 }
+
+#' get_connection_JDBC Connects to a database server using JDBC
+#'
+#' @param dbhost Hostname name to connect to
+#' @param dbname Databse name to connect to
+#' @param dbcp Class path for driver
+#' @param user Username
+#' @param password Password
+#'
+#' @return DBI conection object
+#' @export get_connection_JDBC
+#'
+#' @examples conn <- HMSr::get_connection_JDBC(dbcp="D:/R/Drivers/mssql-jdbc-7.2.1.jre8.jar',user="Bob",password ="Password01")
 
 get_connection_JDBC <- function(dbhost = "sql02", dbname="HgrDwh",dbcp="D:/R/Drivers/mssql-jdbc-7.2.1.jre8.jar",user,password){
   drv <- JDBC(driverClass="com.microsoft.sqlserver.jdbc.SQLServerDriver", classPath=dbcp)
